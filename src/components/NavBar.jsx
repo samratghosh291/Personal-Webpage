7d0ceb22ca82c5006afad93f01e443cc9faeaa11
samratghosh291 <samratghosh490@gmail.com>
2024-05-29 12:53:20 +0530

    upload directory folder structure setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import { BsBorder } from 'react-icons/bs';
+import logo from '../assets/kevinRushLogo.png';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaInstagram } from 'react-icons/fa';
+
+const NavBar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img className='mx-2 w-10' src={logo} alt="logo" />
+        </div>
+
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin/>
+            <FaGithub/>
+            <FaTwitterSquare/>
+            <FaInstagram/>
+        </div>
+    </nav>
+  )
+}
+
+export default NavBar
